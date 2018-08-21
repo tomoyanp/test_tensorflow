9d2a3d096d2d242e88db0e3d3333413b52689c88
tomoyanpy <tomoyanpy@gmail.com>
2018-08-21 11:17:00 +0900

    modi

--- mock_main.py
+++ mock_main.py
@@ -17,6 +17,9 @@ from keras.layers import Dropout
 
 import json
 
+from get_indicator import getBollingerWrapper
+from mysql_connector import MysqlConnector
+
 def build_model(inputs, output_size, neurons, activ_func="linear", dropout=0.25, loss="mae", optimizer="adam"):
     model = Sequential()
 
@@ -48,143 +51,60 @@ def date_string(date):
     return date.strftime('%Y/%m/%d %H:%M:%S')
 
 
-############################
-#
-# MOCK PROGRAM 
-#
-#ACCOUNT_ID = "2542764"
-#ACCESS_TOKEN = "cb570464152b22d04da3f0f5cad2ddd4-0d543f436361df398e1b2ffa6daf227d"
-#ENV = "practice"
-#
-#oanda = oandapy.API(environment=ENV, access_token=ACCESS_TOKEN)
-#
-#response = oanda.get_history(instrument="USD_JPY", granularity="D")
+def getDailyIndicator(base_time, con, span):
+    ### get daily dataset
+    instrument = "GBP_JPY"
+    target_time = base_time - timedelta(days=1)
+    sql = "select max_price, min_price, start_price, end_price from %s_%s_TABLE where insert_time < \'%s\' order by insert_time desc limit %s" % (instrument, "day", target_time, span) 
+    response = con.select_sql(sql)
+    max_price_list = []
+    min_price_list = []
+    start_price_list = []
+    end_price_list = []
+    for res in response:
+        max_price_list.append(res[0])
+        min_price_list.append(res[1])
+        start_price_list.append(res[2])
+        end_price_list.append(res[3])
 
-file = open("output.json", "r")
-response = json.load(file)
+    max_price_list.reverse()
+    min_price_list.reverse()
+    start_price_list.reverse()
+    end_price_list.reverse()
+    
+    sma1d20_list = []
+    time_list = []
 
-#
-#
-#############################
+    for i in range(0, span):
+        tmp_time = target_time - timedelta(days=i)
+        dataset = getBollingerWrapper(tmp_time, instrument,  table_type="day", window_size=20, connector=con, sigma_valiable=2, length=0)
+        sma1d20_list.append(dataset["base_lines"][-1])
+        time_list.append(tmp_time)
 
-#print(response)
-#print("============================")
+    sma1d20_list.reverse()
+    time_list.reverse()
 
-#print(response["candles"])
-#print("============================")
-res = pd.DataFrame(response["candles"])
-#print(res.head())
+    df = pd.DataFrame([])
 
+    df["time"] = time_list
+    df["max"] = max_price_list
+    df["min"] = min_price_list
+    df["start"] = start_price_list
+    df["end"] = end_price_list
+    df["sma1d20"] = sma1d20_list
 
-res["time"] = res["time"].apply(lambda x: iso_jp(x))
-res["time"] = res["time"].apply(lambda x: date_string(x))
-
-#print(res.head())
-
-#print(res.shape)
-#print(res.columns)
-
-#print(res['time'])
-df = res[['time', 'openAsk', 'closeAsk', 'highAsk', 'lowAsk', 'volume']]
-#print(df.columns)
-
-df.columns = ['time', 'openAsk', 'closeAsk', 'highAsk', 'lowAsk', 'volume']
-#print(df.columns)
-
-train, test = df[:400], df[400:]
-
-#train = train * 100
-#test = test * 100
-
-del train["time"]
-del test["time"]
-
-#print(train)
-#print(test)
-
-window_len = 21
-
-train_lstm_in = []
-
-#print(train.columns)
-#print(test.columns)
+    return df
+    
 
 
-for i in range(len(train) - window_len):
-    temp = train[i:(i+window_len)].copy()
-    #print(temp)
-    #print("=======================")
+connector = MysqlConnector()
+base_time = "2018-08-01 00:00:00"
+base_time = datetime.strptime(base_time, "%Y-%m-%d %H:%M:%S")
+span = 5
 
-    for col in train:
-        #print(col)
-        #print("=======================")
-        #print(temp[col])
-        #print("=======================")
-        #print(temp[col].iloc[0])
-        temp.loc[:, col] = temp[col]/temp[col].iloc[0] - 1
-        #print(temp)
-    train_lstm_in.append(temp)
+df = getDailyIndicator(base_time, con, span)
+print df
 
-train_lstm_out = (train["closeAsk"][window_len:].values/train["closeAsk"][:-window_len].values) - 1
-#train_lstm_out = (train[:][window_len:].values/train[:][:-window_len].values) - 1
+numpy_list = df.values
 
-
-test_lstm_in = []
-for i in range(len(test) - window_len):
-    temp = test[i:(i+window_len)].copy()
-    for col in test:
-        temp.loc[:, col] = temp[col]/temp[col].iloc[0] - 1
-
-    test_lstm_in.append(temp)
-
-test_lstm_out = (test["closeAsk"][window_len:].values/test["closeAsk"][:-window_len].values) - 1
-
-#print(train_lstm_in)
-
-train_lstm_in = [np.array(train_lstm_input) for train_lstm_input in train_lstm_in]
-train_lstm_in = np.array(train_lstm_in)
-
-test_lstm_in = [np.array(test_lstm_input) for test_lstm_input in test_lstm_in]
-test_lstm_in = np.array(test_lstm_in)
-
-#print(train_lstm_in)
-
-np.random.seed(202)
-yen_model = build_model(train_lstm_in, output_size=1, neurons=20)
-
-print(train_lstm_in.shape)
-print(train_lstm_out.shape)
-
-#print(train_lstm_out)
-#print(train_lstm_in)
-
-yen_history = yen_model.fit(train_lstm_in, train_lstm_out, epochs=50, batch_size=1, verbose=2, shuffle=True)
-
-### paint epoch, loss rate
-#
-#fig, ax1 = plt.subplots(1,1)
-#ax1.plot(yen_history.epoch, yen_history.history["loss"])
-#ax1.set_title("TrainingError")
-#
-#if yen_model.loss == "mae":
-#    ax1.set_ylabel("Mean Absolute Error(MAE)", fontsize=12)
-#else:
-#    ax1.set_ylabel("Model Loss", fontsize=12)
-#ax1.set_xlabel("# Epochs", fontsize=12)
-#plt.show()
-#
-
-#result = (yen_model.predict(train_lstm_in)+1) * train["closeAsk"].values[:-window_len]
-
-#print(result[0])
-#print(len(result[0]))
-
-#pd_result = pd.DataFrame(result[0])
-#print(pd_result)
-
-
-### paint predict train data
-fig, ax1 = plt.subplots(1,1)
-ax1.plot(df["time"][window_len:].astype(datetime), train["closeAsk"][window_len:], label="Actual", color="blue")
-ax1.plot(df["time"][window_len:].astype(datetime), ((np.transpose(yen_model.predict(train_lstm_in))+1) * train["closeAsk"].values[:-window_len])[0], label="Actual", color="blue")
-
+#print numpy_list
