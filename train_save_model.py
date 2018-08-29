411bc8759bed788fc9409eedcafdfa8e16ff2019
tomoyanpy <tomoyanpy@gmail.com>
2018-08-29 18:32:23 +0900

    modi

--- train_save_model.py
+++ train_save_model.py
@@ -1,81 +1,28 @@
 # coding: utf-8
 
-import pandas as pd
+from train_save_model_wrapper import train_save_model_wrapper
 
 
-pd.set_option("display.max_colwidth", 1000)
-pd.set_option("display.max_columns", None)
-pd.set_option("display.max_rows", None)
+base_time = "2017-07-01 00:00:00"
 
-import numpy as np
-
-np.set_printoptions(threshold=np.inf)
-
-import seaborn as sns
-import matplotlib
-#matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-plt.switch_backend("agg")
-import oandapy
-import configparser
-import datetime
-import pytz
-from datetime import datetime, timedelta
-
-from keras.models import Sequential
-from keras.layers import Activation, Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-
-from sklearn.preprocessing import MinMaxScaler
-
-import json
-
-from get_indicator import getBollingerWrapper
-from mysql_connector import MysqlConnector
-
-from tensor_lib import change_to_ptime, getDataset, change_to_normalization, createTrainDataset, build_model
-
-connector = MysqlConnector()
-train_base_time = change_to_ptime(base_time="2018-07-01 00:00:00")
+# daily train
+table_layout = "day"
 output_train_index = 1
-original_dataset, value_dataset = getDataset(train_base_time, connector, window_size=30, learning_span=300, output_train_index=1, table_layout="day")
-df = pd.DataFrame(value_dataset.copy())
+learning_span = 200
+window_size = 60
+model_filename = "model_1d.json"
+weight_filename = "weight_1d.hdf5"
+normalization_filename = "normalization_1d.json"
 
-max_price = max(original_dataset["end"])
-min_price = min(original_dataset["end"])
+train_save_model_wrapper(base_time, table_layout, output_train_index, learning_span, window_size, model_filename, weight_filename, normalization_filename)
 
-value_dataset = change_to_normalization(value_dataset)
-input_train_data, output_train_data, time_list = createTrainDataset(value_dataset, original_dataset, window_size=30, learning_span=300, output_train_index=1)
+# 1h train
+table_layout = "1h"
+output_train_index = 8
+learning_span = 24*200
+window_size = 24*5
+model_filename = "model_1h.json"
+weight_filename = "weight_1h.hdf5"
+normalization_filename = "normalization_1h.json"
 
-
-np.random.seed(202)
-model = build_model(input_train_data, output_size=1, neurons=200)
-history = model.fit(input_train_data, output_train_data, epochs=500, batch_size=1, verbose=2, shuffle=True)
-
-# モデルの保存
-json_string = model.to_json()
-open("model.json", "w").write(json_string)
-model.save_weights("model_weights.hdf5")
-
-
-
-## 訓練データで予測
-train_predict = model.predict(input_train_data)
-paint_predict = []
-paint_right = []
-for i in range(len(train_predict)):
-    print(time_list[i])
-    paint_predict.append((train_predict[i]*(max_price-min_price))+min_price)
-    print((train_predict[i]*(max_price-min_price))+min_price)
-    paint_right.append((output_train_data[i]*(max_price-min_price))+min_price)
-    print((output_train_data[i]*(max_price-min_price))+min_price)
-
-
-### paint predict train data
-fig, ax1 = plt.subplots(1,1)
-ax1.plot(time_list, paint_predict, label="Predict", color="blue")
-ax1.plot(time_list, paint_right, label="Actual", color="red")
-
-plt.savefig('figure.png')
-
+train_save_model_wrapper(base_time, table_layout, output_train_index, learning_span, window_size, model_filename, weight_filename, normalization_filename)
--- train_save_model_bk.py
+++ train_save_model_bk.py
@@ -2,212 +2,42 @@
 
 import pandas as pd
 
-
 pd.set_option("display.max_colwidth", 1000)
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 
 import numpy as np
-
 np.set_printoptions(threshold=np.inf)
-
-import seaborn as sns
-import matplotlib
-#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.switch_backend("agg")
-import oandapy
-import configparser
-import datetime
-import pytz
-from datetime import datetime, timedelta
 
-from keras.models import Sequential
-from keras.layers import Activation, Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-
-from sklearn.preprocessing import MinMaxScaler
-
-import json
-
-from get_indicator import getBollingerWrapper
 from mysql_connector import MysqlConnector
-
-def build_model(inputs, output_size, neurons, activ_func="linear", dropout=0.25, loss="mae", optimizer="adam"):
-    model = Sequential()
-
-    model.add(LSTM(neurons, input_shape=(inputs.shape[1], inputs.shape[2])))
-    model.add(Dropout(dropout))
-    model.add(Dense(units=output_size))
-    model.add(Activation(activ_func))
-    model.compile(loss=loss, optimizer=optimizer)
-
-    return model
-
-
-def iso_jp(iso):
-    date = None
-    try:
-        date = datetime.strptime(iso, '%Y-%m-%dT%H:%M:%S.%fZ')
-        date = pytz.utc.localize(date).astimezone(pytz.timezone("Asia/Tokyo"))
-    except ValueError:
-        try:
-            date = datetime.strptime(iso, '%Y-%m-%dT%H:%M:%S.%f%z')
-            date = dt.astimezone(pytz.timezone("Asia/Tokyo"))
-        except ValueError:
-            pass
-    return date
- 
-def date_string(date):
-    if date is None:
-        return ''
-    return date.strftime('%Y/%m/%d %H:%M:%S')
-
-
-# 必要なデータを取得してDataFrameに突っ込んで返す
-def getDataSet(base_time, con, window_size, learning_span, output_train_index):
-    ### get daily dataset
-
-    length = learning_span + output_train_index
-
-    instrument = "GBP_JPY"
-    target_time = base_time - timedelta(days=1)
-    sql = "select max_price, min_price, start_price, end_price, insert_time from %s_%s_TABLE where insert_time < \'%s\' order by insert_time desc limit %s" % (instrument, "day", target_time, length) 
-    response = con.select_sql(sql)
-    #print(sql)
-
-    max_price_list = []
-    min_price_list = []
-    start_price_list = []
-    end_price_list = []
-    time_list = []
-    for res in response:
-        max_price_list.append(res[0])
-        min_price_list.append(res[1])
-        start_price_list.append(res[2])
-        end_price_list.append(res[3])
-        time_list.append(res[4])
-
-    max_price_list.reverse()
-    min_price_list.reverse()
-    start_price_list.reverse()
-    end_price_list.reverse()
-    time_list.reverse()
-    
-    sma1d20_list = []
-    i = 0
-    while len(end_price_list) != len(sma1d20_list):
-        tmp_time = target_time - timedelta(days=i)
-        dataset = getBollingerWrapper(tmp_time, instrument,  table_type="day", window_size=20, connector=con, sigma_valiable=2, length=0)
-        try:
-            sma1d20_list.append(dataset["base_lines"][-1])
-        except Exception as e:
-            pass
-
-        i = i + 1
-
-        #print(len(end_price_list))
-        #print(len(sma1d20_list))
-
-    sma1d20_list.reverse()
-
-    original_dataset = {"end": end_price_list,
-               "start": start_price_list,
-               "max": max_price_list,
-               "min": min_price_list,
-               "sma1d20": sma1d20_list,
-               "time": time_list}
-
-    value_dataset = {"end": end_price_list,
-               "start": start_price_list,
-               "max": max_price_list,
-               "min": min_price_list,
-               "sma1d20": sma1d20_list}
-
-    return original_dataset, value_dataset
-
-def change_to_normalization(dataset):
-    tmp_df = pd.DataFrame(dataset)
-    np_list = np.array(tmp_df)
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    normalization_list = scaler.fit_transform(np_list)
-
-    return normalization_list
-
- # 引数で与えられたndarrayをwindow_sizeで分割して返す(ndarray)
-def createDataset(dataset, original_dataset, window_size, learning_span, output_train_index, output_flag):
-    input_train_data = []
-    output_train_data = []
-    time_list = []
-    for i in range(0, learning_span-(window_size+1)):
-
-        temp = []
-        temp_index = 0
-        for k in range(i, i+window_size):
-            temp.append(dataset[k].copy())
-            temp_index = k
-
-        input_train_data.append(temp)
-        try:
-            if output_flag:
-                output_train_data.append(dataset[temp_index+output_train_index][0].copy())
-                time_list.append(original_dataset["time"][temp_index+output_train_index])
-                  
-            else:
-                pass
-
-        except Exception as e:
-            pass
-
-    input_train_data = np.array(input_train_data)
-    output_train_data = np.array(output_train_data)
-    time_list = np.array(time_list)
-
-    return input_train_data, output_train_data, time_list
-
-
-def change_to_ptime(base_time):
-    base_time = datetime.strptime(base_time, "%Y-%m-%d %H:%M:%S")
-    return base_time
-
-
-def join_dataframe(sdf, ddf):
-    index = 0
-    for col in ddf:
-        sdf = sdf.rename(columns={index: col})
-        index = index + 1
-    
-    ddf = ddf.append(sdf, ignore_index=True)
-
-    return ddf
+from tensor_lib import change_to_ptime, getDataset, change_to_normalization, createTrainDataset, build_model
 
 connector = MysqlConnector()
 train_base_time = change_to_ptime(base_time="2018-07-01 00:00:00")
-output_train_index = 1
-original_dataset, value_dataset = getDataSet(train_base_time, connector, window_size=30, learning_span=300, output_train_index=1)
+
+table_layout = "1h"
+output_train_index = 8
+learning_span = 24*120 # 半年分
+window_size = 24
+original_dataset, value_dataset = getDataset(train_base_time, connector, window_size, learning_span, output_train_index, table_layout)
 df = pd.DataFrame(value_dataset.copy())
 
 max_price = max(original_dataset["end"])
 min_price = min(original_dataset["end"])
 
 value_dataset = change_to_normalization(value_dataset)
-input_train_data, output_train_data, time_list = createDataset(value_dataset, original_dataset, window_size=30, learning_span=300, output_train_index=1, output_flag=True)
-
-
+input_train_data, output_train_data, time_list = createTrainDataset(value_dataset, original_dataset, window_size, learning_span, output_train_index)
 
 np.random.seed(202)
-print(input_train_data.shape)
-print(output_train_data.shape)
-model = build_model(input_train_data, output_size=1, neurons=20)
-history = model.fit(input_train_data, output_train_data, epochs=50, batch_size=1, verbose=2, shuffle=True)
+model = build_model(input_train_data, output_size=1, neurons=200)
+history = model.fit(input_train_data, output_train_data, epochs=500, batch_size=1, verbose=2, shuffle=True)
 
 # モデルの保存
 json_string = model.to_json()
 open("model.json", "w").write(json_string)
 model.save_weights("model_weights.hdf5")
-
-
 
 ## 訓練データで予測
 train_predict = model.predict(input_train_data)
@@ -219,7 +49,6 @@ for i in range(len(train_predict)):
     print((train_predict[i]*(max_price-min_price))+min_price)
     paint_right.append((output_train_data[i]*(max_price-min_price))+min_price)
     print((output_train_data[i]*(max_price-min_price))+min_price)
-
 
 ### paint predict train data
 fig, ax1 = plt.subplots(1,1)
